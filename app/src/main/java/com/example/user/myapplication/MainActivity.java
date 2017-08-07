9fa1ac1914a5feca043865c92f8cb5d899a71df7
silpajayaraj <silpajayaraj@gmail.com>
2017-08-07 15:57:34 +0530

    changes

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,87 +1,117 @@
 package com.example.user.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
-import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import static com.example.user.myapplication.R.*;
+import com.example.user.myapplication.GraphActivity;
+import com.example.user.myapplication.sgfilter.SGFilter;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
 
-public class MainActivity extends Activity implements SensorEventListener{
-    private TextView xText,yText,zText;
-    String sx,sy,sz;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    TextView title, tvx, tvy, tvz;
+    Button btnRead, btnStart, btnStop;
+    RelativeLayout layout;
 
-    private Sensor mysensor;
-    private SensorManager SM;
-}
+    private ArrayList<Float> sensorXData, sensorYData, sensorZData, timeData;
+    private  double[] filteredXData;
+
+    private BufferedWriter mBufferedWriter;
+    private BufferedReader mBufferedReader;
+    private float x;
+    private float y;
+    private float z;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-// create sensor manager
-        SM=(SensorManager)getSystemService(SENSOR_SERVICE);
-        //accelerometer sensor
-        mysensor =SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        //register sensor listener
-        SM.registerListener(this,mysensor,SensorManager.SENSOR_DELAY_NORMAL);
-        //Assign textview
-        xText=(TextView)findViewById(id.textView2);
-        yText=(TextView)findViewById(id.textView3);
-        zText=(TextView)findViewById(id.textView4);
-        SM
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        //get layout
+        // layout = (RelativeLayout) findViewById(R.id.relative);
+
+        //get textviews
+        title = (TextView) findViewById(R.id.textView1);
+        tvx = (TextView) findViewById(R.id.textView2);
+        tvy = (TextView) findViewById(R.id.textView3);
+        tvz = (TextView) findViewById(R.id.textView4);
+
+        btnStart = (Button) findViewById(R.id.button1);
+        btnStop = (Button) findViewById(R.id.button2);
+        btnRead = (Button) findViewById(R.id.button3);
+
+        sensorXData = new ArrayList<Float>();
+        sensorYData = new ArrayList<Float>();
+        sensorZData = new ArrayList<Float>();
+
+        //filteredXData = new double[10];
 
 
+        btnRead.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                SGolayFilter();
 
+                Intent i = new Intent(getApplicationContext(), GraphActivity.class);
+                //i.putExtra("sensorXData", sensorXData);
+                //i.putExtra("sensorYData", sensorYData);
+
+                //i.putExtra("sensorZData", sensorZData);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            x = sensorEvent.values[0];
+            y = sensorEvent.values[1];
+            z = sensorEvent.values[2];
+
+            sensorXData.add(x);
+            sensorYData.add(y);
+            sensorZData.add(z);
+
+            // acc= String.valueOf(x) + ", " + String.valueOf(y) + ", " + String.valueOf(z);
+
+            tvx.setText("X = " + String.valueOf(x));
+            tvy.setText("Y = " + String.valueOf(y));
+            tvz.setText("Z = " + String.valueOf(z));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 
-
+    public void SGolayFilter() {
+        SGFilter filter = new SGFilter(5, 5);
+        double[] coefficients = SGFilter.computeSGCoefficients(5, 5, 4);
+        double array[] = new double[sensorXData.size()];
+        for (int i = 0; i < sensorXData.size(); i++)
+            array[i] = Double.parseDouble(Float.toString(sensorXData.get(0)));
+        filteredXData = filter.smooth(array, coefficients);
+    }
 }
-public class Activity1 : Activity, ISensorEventListener
-        {
-static readonly object _syncLock = new object();
-        SensorManager _sensorManager;
-        TextView _sensorTextView;
 
-    public void OnAccuracyChanged(Sensor sensor, SensorStatus accuracy)
-    {
-        // We don't want to do anything here.
-    }
-
-    public void OnSensorChanged(SensorEvent e)
-    {if(event.)
-        lock (_syncLock)
-        {
-            _sensorTextView.Text = string.Format("x={0:f}, y={1:f}, z={2:f}", e.Values[0], e.Values[1], e.Values[2]);
-        }
-    }
-protected override void OnCreate(Bundle savedInstanceState)
-        {
-        base.OnCreate(savedInstanceState);
-        SetContentView(Resource.Layout.Main);
-        _sensorManager = (SensorManager) GetSystemService(SensorService);
-        _sensorTextView = FindViewById<TextView>(Resource.Id.accelerometer_text);
-        }
-
-protected override void OnResume()
-        {
-        base.OnResume();
-        _sensorManager.RegisterListener(this,
-        _sensorManager.GetDefaultSensor(SensorType.Accelerometer),
-        SensorDelay.Ui);
-        }
-
-protected override void OnPause()
-        {
-        base.OnPause();
-        _sensorManager.UnregisterListener(this);
-        }
